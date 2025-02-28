969f8bd20ae27e78a5c6d0aa1805b27db3c13c24
Leslie Lamarche <leslie.lamarche@sri.com>
2025-02-28 02:10:35 -0800

    Convert parse_novatel to class and add gps2utc function to utils.

--- src/gnss_scintillation/parse/utils.py
+++ src/gnss_scintillation/parse/utils.py
@@ -12,15 +12,15 @@ def twos_comp(val, bits):
     return val                         # return positive value as is
 
 
-def gps2utc(wnc, tow):
-    # Convert GPS timestamps to numpy.datetim64 array
-
-    # tstmp = np.datetime64('1980-01-06') + wnc.astype('timedelta64[W]') + (tow*1000.).astype('timedelta64[ms]')
-    # tm = spt.Ticktock(tstmp.astype('datetime64[s]').astype(int), 'UNX')
-    # utc_tstmp = tstmp-(tm.leaps.astype('timedelta64[s]')-19)
-
-    tm = spt.Ticktock(wnc*7*24*60*60+tow, 'GPS')
-    utc_tstmp = np.array(tm.UTC, dtype='datetime64')
-    
-    return utc_tstmp
+#def gps2utc(wnc, tow):
+#    # Convert GPS timestamps to numpy.datetim64 array
+#
+#    # tstmp = np.datetime64('1980-01-06') + wnc.astype('timedelta64[W]') + (tow*1000.).astype('timedelta64[ms]')
+#    # tm = spt.Ticktock(tstmp.astype('datetime64[s]').astype(int), 'UNX')
+#    # utc_tstmp = tstmp-(tm.leaps.astype('timedelta64[s]')-19)
+#
+#    tm = spt.Ticktock(wnc*7*24*60*60+tow, 'GPS')
+#    utc_tstmp = np.array(tm.UTC, dtype='datetime64')
+#    
+#    return utc_tstmp
 
