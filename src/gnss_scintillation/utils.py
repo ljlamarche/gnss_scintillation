925f10e14c6478b7537ca664728c12492a18384c
Leslie Lamarche <leslie.lamarche@sri.com>
2025-03-01 00:05:19 -0800

    Clean up IPP calculation code in utils.

--- src/gnss_scintillation/utils.py
+++ src/gnss_scintillation/utils.py
@@ -5,46 +5,65 @@ import numpy as np
 import datetime as dt
 import pymap3d as pm
 
-#def projalt(site,az,el,proj_alt=300.):
-#
-#    lat0, lon0, alt0 = site
-#    az = az*np.pi/180.
-#    el = el*np.pi/180.
-#
-#    x, y, z = pm.geodetic2ecef(lat0, lon0, alt0)
-#    vx, vy, vz = pm.enu2uvw(np.cos(el)*np.sin(az), np.cos(el)*np.cos(az), np.sin(el), lat0, lon0)
-#
-#    earth = pm.Ellipsoid()
-#    a2 = (earth.semimajor_axis + proj_alt*1000.)**2
-#    b2 = (earth.semimajor_axis + proj_alt*1000.)**2
-#    c2 = (earth.semiminor_axis + proj_alt*1000.)**2
-#
-#    A = vx**2/a2 + vy**2/b2 + vz**2/c2
-#    B = x*vx/a2 + y*vy/b2 + z*vz/c2
-#    C = x**2/a2 + y**2/b2 + z**2/c2 -1
-#
-#    alpha = (np.sqrt(B**2-A*C)-B)/A
-#
-#    lat, lon, alt = pm.ecef2geodetic(x + alpha*vx, y + alpha*vy, z + alpha*vz)
-#
-#    return lat, lon, alt/1000.
 
 def gps2utc(wnc, tow):
+    # THIS FUNCTION DOES NOT HANDLE LEAP SECONDS CORRECTLY
+    # There is a way to do this with spacepy, but it is extremely slow and will sometimes crash because it runs out of memory
+    # Until this is solved, output will be off by ~18 seconds
     gps_tstmp = (np.array(wnc)*7*24*60*60*1000+np.array(tow))
     gps_epoch = np.datetime64('1980-01-06').astype('datetime64[ms]').astype(int)
     time = (gps_epoch + gps_tstmp).astype('datetime64[ms]')
     return time
 
 
-def calc_ipp(site, sat_ephem, height=300.):
+def calc_ipp(site, satellite, satcoords='azel', height=300.):
+    '''
+    Calculate the ionospheric pierce point (IPP) based on receiver and satellite coordinates.
+    Satellite coordinates can be provided as azel, ecef, or geodetic.
+
+    Input
+    -----
+    site: ground receiver site coordinates as a 3 element list/array [geodetic latitude, geodetic longitude, geodetic altitude]
+    satellite: position of the GNSS satellite in one of three coordinates
+        - azimuth/elevation: [azimuth, elevation] in degrees (note that only two coordinates are required for this option
+        - Earth-Cenetered Earth Fixed: [X, Y, Z] in meters
+        - geodetic: [geodetic latitude, geodetic longitude geodetic altitude] - altitude in meters
+    satcoords: keyword to indicate what coordinates the satellite postion is provided  in (default='azel')
+        - azimuth/elevation: 'azel'
+        - Earth-Cenered Earth-Fixed: 'ecef'
+        - geodetic: 'geo'
+
+    Returns
+    -------
+    lat: geodetic latitude of the IPP
+    lon: geodetic longitude of the IPP
+    '''
+
+    # Get site coordinate in ECEF
     lat0, lon0, alt0 = site
     x, y, z = pm.geodetic2ecef(lat0, lon0, alt0)
 
-    vx = sat_ephem[0] - x
-    vy = sat_ephem[1] - y
-    vz = sat_ephem[2] - z
+    # Calcualte vector from site to satellite
+    #   Note that this does not actually have to be normalized
+    if satcoords == 'azel':
+        e, n, u = pm.aer2enu(*satellite, 1.)
+        vx, vy, vz = pm.enu2uvw(e, n, u, lat0, lon0)
 
-    earth = pm.Ellipsoid()
+    elif satcoords == 'ecef':
+        vx = satellite[0] - x
+        vy = satellite[1] - y
+        vz = satellite[2] - z
+
+    elif satcoords == 'geo':
+        sx, sy, sz = pm.geodetic2ecef(*satellite)
+        vx = sx - x
+        vy = sy - y
+        vz = sz - z
+
+    else:
+        raise ValueError(f'satcoords={satcoords} is not a valid coordinate option')
+
+    earth = pm.Ellipsoid.from_name('wgs84')
     a2 = (earth.semimajor_axis + height*1000.)**2
     b2 = (earth.semimajor_axis + height*1000.)**2
     c2 = (earth.semiminor_axis + height*1000.)**2
@@ -57,7 +76,7 @@ def calc_ipp(site, sat_ephem, height=300.):
 
     lat, lon, alt = pm.ecef2geodetic(x + alpha*vx, y + alpha*vy, z + alpha*vz)
 
-    return lat, lon, alt/1000.
+    return lat, lon
 
 #def gaussiran_method(site, az, el, height=300.):
 #
